53f2732cfe89a0e89fb898adadccca71c751316e
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-21 17:16:31 +0100

    Esercizio completato con bonus

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
 /*Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.*/
 
-// DOM
-const myContainer = document.querySelector(".container");
-
 
 // Creare l’array di oggetti con le informazioni e stampa in console
 const teamData = [
     {
         "name" : "Wayne Barnett",
         "role" : "Founder & CEO",
-        "img" : "wayne-barnett-founder-ceo.jpg"
+        "img" : "img/wayne-barnett-founder-ceo.jpg"
     },
     {
         "name" : "Angela Caroll",
         "role" : "Chief Editor",
-        "img" : "angela-caroll-chief-editor.jpg"
+        "img" : "img/angela-caroll-chief-editor.jpg"
     },
     {
         "name" : "Walter Gordon",
         "role" : "Office Manager",
-        "img" : "walter-gordon-office-manager.jpg"
+        "img" : "img/walter-gordon-office-manager.jpg"
     },
     {
         "name" : "Angela Lopez",
         "role" : "Social Media Manager",
-        "img" : "angela-lopez-social-media-manager.jpg"
+        "img" : "img/angela-lopez-social-media-manager.jpg"
     },
     {
         "name" : "Scott Estrada",
@@ -34,18 +31,32 @@ const teamData = [
     {
         "name" : "Barbara Ramos",
         "role" : "Grapic Designer",
-        "img" : "barbara-ramos-graphic-designer.jpg"
+        "img" : "img/barbara-ramos-graphic-designer.jpg"
     }
 ];
 
 console.log(teamData);
 
-// stampa dei dati su DOM
-for (let i = 0; i < teamData.length; i++) {
-    let teamiesimo = teamData[i];
-    let bottomCards = document.querySelector(".bottom-cards");
-    let divInfo = document.createElement("div");
-    divInfo.innerHTML = `${teamiesimo.name} ${teamiesimo.role} ${teamiesimo.img}`;
-    bottomCards.append(divInfo);
-    console.log(bottomCards)
-}
+data();
+
+// funzione per creare i div contenenti le img e i dati
+function data() {
+    for (let i = 0; i < teamData.length; i++) {
+        let cards =`
+            <div class="cards">
+                 <div class="top-cards">
+                    <img src="${teamData[i].img}" alt="">
+                 </div>
+                 <div class="bottom-cards">
+                     <div>${teamData[i].name}</div>
+                     <div>${teamData[i].role}</div>
+                 </div>
+            </div>
+        `
+     
+        let myContainer = document.querySelector("div.container").innerHTML += cards;
+    }
+};
+
+
+
